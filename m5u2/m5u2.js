ae1b6bdd262d0e3b835af8fe1dc219256cfd5c5e
Dmitriy Bezgodko <daimon.94@mail.ru>
2022-08-31 14:01:41 +0300

    add js m5u2

---
+++ m5u2/m5u2.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const formTittle = document.getElementsByClassName('form__title-box');
+
+const productID = document.getElementsByClassName('text-gray');
+
+const productBtnID = document.getElementsByClassName('form__button-id');
+
+const newProductForm = document.getElementsByClassName('form__box');
+
+const checkbox = document.getElementsByClassName('form__checkbox');
+
+const textDiscont = document.getElementsByClassName('form__text-input_type_discont');
+
+const finishPrice = document.getElementsByClassName('form__bold-text');
