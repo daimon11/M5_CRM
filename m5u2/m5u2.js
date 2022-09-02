292ce1ae03e180392693188be3c992093bfa7b55
Dmitriy Bezgodko <daimon.94@mail.ru>
2022-09-02 16:19:43 +0300

    first

---
+++ m5u2/m5u2.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const formTittle = document.querySelector('.form__title-box');
+
+const productID = document.querySelector('.text-gray');
+
+const productBtnID = document.querySelector('.form__button-id');
+
+const newProductForm = document.querySelector('.form__box');
+
+const checkbox = document.querySelector('.form__checkbox');
+
+const textDiscont = document.querySelector('.form__text-input_type_discont');
+
+const finishPrice = document.querySelector('.form__bold-text');
